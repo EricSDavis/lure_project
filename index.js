d65afd6e1b24a027ccf19d85cc15b7b7d4433a2d
caoyr03 <42782107+caoyr03@users.noreply.github.com>
2019-10-25 11:36:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -57,8 +57,6 @@ function makeTable(data, colNames) {
     return table;
 }
 
-// Define function to enable zoom, source https://bl.ocks.org/mbostock/34f08d5e11952a80609169b7917d4172
-
 // d3 v5 uses promises to read in data, sources: http://datawanderings.com/2018/08/15/d3-js-v5-promise-syntax-examples/
 $(document).ready(function() {
     d3.csv("probeData.csv").then(function(data){
@@ -66,15 +64,17 @@ $(document).ready(function() {
 
         // Define plotting parameters
         var margin = {top: 10, right: 30, bottom: 30, left: 40};
-        var width = 1000 - margin.left - margin.right;
+        var width = 1400 - margin.left - margin.right;
         var height = 400 - margin.top - margin.bottom;
         var xmin = d3.min(data, function(d) { return +d.start; });
         var xmax = d3.max(data, function(d) { return +d.stop; });
         var ymin = 0;
         var ymax = 1;
         var fillLevels = ["High", "Medium", "Low"];
+        var legendRectSize = 8;                                  
+        var legendSpacing = 4; 
 
-        console.log(width);
+        //console.log(width);
 
         // Define x scaling function
         var x = d3.scaleLinear()
@@ -90,29 +90,74 @@ $(document).ready(function() {
         var quality = d3.scaleOrdinal()
             .domain(fillLevels)
             .range(["#138fc8", "#5fcef2", "#b6e9fa"]);
-                
+            
+
+        // Define the zoom variable, scale and call function
+        var zoom = d3.zoom()
+            .extent([[0, 0], [width,height]])
+            .scaleExtent([1, 20])
+            .on("zoom", updateChart);
+     
         // Create svg element
         var regionPlot = d3.select("body")
             .append("svg")
                 .attr("width", width + margin.left + margin.right)
                 .attr("height", height + margin.top + margin.bottom)
+                .call(zoom)
             .append("g")
                 .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            
+        // Append y-axis gridline
+        regionPlot.append("g")
+        	.attr("class", "grid")
+      		.call(d3.axisLeft(y).ticks(5,"s")
+      		.tickSize(-width)
+          	.tickFormat(""));
 
-        // Append y-axis
-        regionPlot
-            .append("g")
-            .call(d3.axisLeft(y).ticks(5,"s"));
-
+		// Append y-axis
+		regionPlot.append("g")
+      		.call(d3.axisLeft(y).ticks(5,"s"));
+      		
         // Append x-axis
-        regionPlot
+        var xaxis = regionPlot
             .append("g")
-                .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x).ticks(5));
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x).ticks(5)
+            );
 
+		// Append legend line
+		regionPlot.append("g")
+  			.attr("class", "legendOrdinal")
+  			.attr("transform", "translate(" + 1200 + "," + margin.top + ")");
+
+		var legendOrdinal = d3.legendColor()
+  			.shape("path", d3.symbol().type(d3.symbolSquare).size(150)())
+  			.shapePadding(10)
+  			.scale(quality);
+
+		regionPlot.select(".legendOrdinal")
+  			.call(legendOrdinal);
+
+            
+          // text label for the y axis
+  		regionPlot.append("text")
+      		.attr("transform", "rotate(-90)")
+      		.attr("y", 0 - margin.left)
+      		.attr("x",0 - (height / 2))
+      		.attr("dy", "1em")
+      		.style("text-anchor", "middle")
+      		.text("GC"); 
+      		
+      		// text label for the x axis
+  		regionPlot.append("text")             
+      		.attr("transform","translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
+      		.style("text-anchor", "middle")
+      		.text("Position");
+           
         // Append rectangles
         regionPlot
             .append("g")
+            .attr("clip-path", "url(#clip)")
                 .selectAll("rect")
                 .data(data)
                 .enter()
@@ -129,22 +174,51 @@ $(document).ready(function() {
                     })
                     //.attr("y", function() { return y(Math.random()); });
                     .attr("y", function(d) { return y(d.GC); })
-                    .attr("fill", function(d) { return quality(d.quality); });
-                
-        
+                    .attr("fill", function(d) { return quality(d.quality); })
+                    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+                    .on("mouseover", function(d){
+                    d3.select(this).attr('fill','orange');
+                    })
+          			.on("mouseout", function(d){
+          			d3.select(this).attr('fill',function(d) { return quality(d.quality); });
+          			});
+            
 
-
+        // Add a clipPath: everything out of this area won't be drawn.
+        var clip = regionPlot.append("defs").append("SVG:clipPath")
+            .attr("id", "clip")
+            .append("SVG:rect")
+            .attr("width", width )
+            .attr("height", height )
+            .attr("x", 0)
+            .attr("y", 0);
+                    
         // Build D3 table
         makeTable(data, colNames);
 
         // Apply bootstrap styling and pagination to table using jquery, source: https://datatables.net/examples/styling/bootstrap
         $('#probeTable').DataTable();
-        
+
+        // Zoom in function updating the graph axis and rect
+        function updateChart() {
+            // recover the new scale
+            var newX = d3.event.transform.rescaleX(x); 
+            // update axes with these new boundaries
+            xaxis.call(d3.axisBottom(newX));
+            // update rect position
+            regionPlot
+            .selectAll("rect")
+            .data(data)
+                .attr('x', function(d) {return newX(+d.start)});
+
+            // New XAxis bound values(min/max): newX.domain()[0],newX.domain()[1], used for filtering data records
+            //console.log(newX.domain()[0]);
+        }
+
 
     
         
     });
 });
-
 
 console.log("End");
